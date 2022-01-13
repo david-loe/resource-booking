7e40e1fe6e9efd6392f5f4de827ff14051e1e3ca
David Loe <d.loewens24@gmail.com>
2022-01-13 17:10:38 +0100

    add ical for 'roomservice'

--- backend/icalRoute.js
+++ backend/icalRoute.js
@@ -5,18 +5,39 @@ const ICAL = require('ical.js')
 router.get('/ical/:name', async (req, res) => {
     if (req.query.token === process.env.VUE_APP_ICAL_TOKEN) {
         if (req.params.name.length > 0) {
-            const room = await Room.findOne({ name: req.params.name })
-            if(room){
-                res.set({"Content-Disposition":"attachment; filename=\""+"calendar"+".ical\""})
-                const comp = new ICAL.Component(room.ical)
-                res.send(ICAL.stringify(room.ical))
-            }else{
-                res.status(400).send({ message: "Room with name: '" + req.params.name + "' not found." })
+            if (req.params.name === "roomservice") {
+                const rooms = await Room.find({})
+                var icals = []
+                for(var room of rooms){
+                    icals.push(room.ical)
+                }
+                const roomServiceIcal = new ICAL.Component(['vcalendar', [], []])
+                for(const ical of icals){
+                    const comp = new ICAL.Component(ical)
+                    const subcomps = comp.getAllSubcomponents()
+                    for(const subcomp of subcomps){
+                        if(subcomp.getFirstPropertyValue('x-room-service').toLowerCase() === 'true'){
+                            roomServiceIcal.addSubcomponent(subcomp)
+                        }
+                    }
+                }
+                res.set({ "Content-Disposition": "attachment; filename=\"" + "calendar" + ".ical\"" })
+                res.send(roomServiceIcal.toString())
+
+            } else {
+                const room = await Room.findOne({ name: req.params.name })
+                if (room) {
+                    res.set({ "Content-Disposition": "attachment; filename=\"" + "calendar" + ".ical\"" })
+                    res.send(ICAL.stringify(room.ical))
+                } else {
+                    res.status(400).send({ message: "Room with name: '" + req.params.name + "' not found." })
+                }
             }
-        }else{
+
+        } else {
             res.status(400).send({ message: "Name Missing" })
         }
-    }else{
+    } else {
         res.status(401).send({ message: "unauthorized" })
     }
 })
