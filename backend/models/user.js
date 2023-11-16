a42ffabc1d9eb72d8a86a4d38832b18d493852be
David Loe <d.loewens24@gmail.com>
2023-11-16 11:48:19 +0100

    index mail

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String },
     isAdmin: { type: Boolean, default: false },
     isService: { type: Boolean, default: false },
-    mail: { type: String },
+    mail: { type: String, index: true },
     settings: {
         initialCalendarView: { type: String, enum: ['next3weeks', 'dayGridMonth', 'listWeek'], default: 'next3weeks' },
         onlyShowAvailableResourcesInOverview: { type: Boolean, default: true }
